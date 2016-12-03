f01432a98768f673dcde381719ef69f6024963ab
Kent Nagata <ngtknt@me.com>
2016-12-04 07:26:07 +0900

    Add vue base files

--- src/main.js
+++ src/main.js
@@ -1,2 +1,16 @@
+import routes from 'routes.js'
+import App from 'App.vue'
+
 // Scss
 import 'stylesheets/app.scss'
+
+// Vue Routing
+Vue.use(VueRouter)
+const router = new VueRouter(routes)
+new Vue({
+  router,
+  template: '<App ref="app" />',
+  components: {
+    'App': App
+  }
+}).$mount('#app')
