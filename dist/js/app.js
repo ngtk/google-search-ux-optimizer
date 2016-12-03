68ba3b497ec712b09cc6f1030aafd44d44a1a334
Kent Nagata <ngtknt@me.com>
2016-12-04 07:26:35 +0900

    Build

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,9 +1,100 @@
 webpackJsonp([0],[
 /* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function(Vue, VueRouter) {'use strict';
+	
+	var _routes = __webpack_require__(4);
+	
+	var _routes2 = _interopRequireDefault(_routes);
+	
+	var _App = __webpack_require__(5);
+	
+	var _App2 = _interopRequireDefault(_App);
+	
+	__webpack_require__(8);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	Vue.use(VueRouter);
+	var router = new VueRouter(_routes2.default);
+	new Vue({
+	  router: router,
+	  template: '<App ref="app" />',
+	  components: {
+	    'App': _App2.default
+	  }
+	}).$mount('#app');
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1), __webpack_require__(2)))
+
+/***/ },
+/* 1 */,
+/* 2 */,
+/* 3 */,
+/* 4 */
 /***/ function(module, exports) {
 
-	// Scss
-	import 'stylesheets/app.scss';
+	"use strict";
+
+/***/ },
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var __vue_exports__, __vue_options__
+	var __vue_styles__ = {}
+	
+	/* script */
+	__vue_exports__ = __webpack_require__(6)
+	
+	/* template */
+	var __vue_template__ = __webpack_require__(7)
+	__vue_options__ = __vue_exports__ = __vue_exports__ || {}
+	if (
+	  typeof __vue_exports__.default === "object" ||
+	  typeof __vue_exports__.default === "function"
+	) {
+	__vue_options__ = __vue_exports__ = __vue_exports__.default
+	}
+	if (typeof __vue_options__ === "function") {
+	  __vue_options__ = __vue_options__.options
+	}
+	
+	__vue_options__.render = __vue_template__.render
+	__vue_options__.staticRenderFns = __vue_template__.staticRenderFns
+	
+	module.exports = __vue_exports__
+
+
+/***/ },
+/* 6 */
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	exports.default = {
+	  name: 'App'
+	};
+
+/***/ },
+/* 7 */
+/***/ function(module, exports) {
+
+	module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;
+	  return _h('div', {
+	    attrs: {
+	      "id": "app"
+	    }
+	  })
+	},staticRenderFns: []}
+
+/***/ },
+/* 8 */
+/***/ function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
 
 /***/ }
 ]);
